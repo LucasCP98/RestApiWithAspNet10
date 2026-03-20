7778e8acc4feb4fd7e569ce712064ca32f3e6aef
LucasCP98 <lucas.pereira@gochange.com.br>
2026-03-20 11:37:45 -0300

    Definindo a interface IPersonService

---
+++ RestApiWithAspNet10/Controllers/PersonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestApiWithAspNet10.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+    }
+}    
---
+++ RestApiWithAspNet10/Controllers/Service/IPersonServices.cs
@@ -0,0 +1,14 @@
+﻿using RestApiWithAspNet10.Controllers.Model;
+
+namespace RestApiWithAspNet10.Controllers.Service
+{
+    public interface IPersonServices
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+
+    }
+}
