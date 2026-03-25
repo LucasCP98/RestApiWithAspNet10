21b73eb85d04aa57916c9ee41df4262935d686f3
LucasCP98 <lucas.pereira@gochange.com.br>
2026-03-25 14:57:55 -0300

    Mapeando a Entidade com Entitity Manager e SQL Server

--- RestApiWithAspNet10/Controllers/Model/Context/MSSQLContext.cs
+++ RestApiWithAspNet10/Controllers/Model/Context/MSSQLContext.cs
@@ -4,9 +4,9 @@ namespace RestApiWithAspNet10.Controllers.Model.Context
 {
     public class MSSQLContext : DbContext
     {
-        public MSSQLContext(DbContextOptions<MSSQLContext> options) : base(options)
-        {
-        }
+        public MSSQLContext(DbContextOptions<MSSQLContext> options) : base(options){ }
+
+        public DbSet<Person> Persons { get; set; }
 
     }
 }
