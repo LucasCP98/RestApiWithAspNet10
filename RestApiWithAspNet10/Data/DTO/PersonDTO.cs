613e03ea0f8596730c77be5c9ebf643efebe3d27
LucasCP98 <lucas.pereira@gochange.com.br>
2026-04-21 20:47:40 -0300

    Integrando Scalar para Documentar APIs no ASP.NET 10

--- RestApiWithAspNet10/Data/DTO/PersonDTO.cs
+++ RestApiWithAspNet10/Data/DTO/PersonDTO.cs
@@ -11,22 +11,22 @@ namespace RestApiWithAspNet10.Data.DTO
         
         // Custom serialização" se aplica quando você precisa mudar alguma coisa no json retornando,
         // por exemplo, a propriedade "FirstName" vai ser serializada como "first_name" no json retornado.
-        [JsonPropertyName("first_name")]
+        //[JsonPropertyName("first_name")]
         public string FirstName { get; set; }
 
         // Ignorar a propriedade "LastName" no json retornado.
         // ou podemos usar o [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         // para ignorar somente quando for null, existe outras condições também.
-        [JsonIgnore]
+        //[JsonIgnore]
         public string LastName { get; set; }
         
         // ordenar ex address sera o 5 lugar.
-        [JsonPropertyOrder(5)]
+        //[JsonPropertyOrder(5)]
         public string Address { get; set; }
 
         // No gender criamos um custom serializer para retornar "M" ou "F" no json,
         // mas internamente na nossa aplicação continuamos usando "Male" e "Female".
-        [JsonConverter(typeof(GenderSerializer))]
+        //[JsonConverter(typeof(GenderSerializer))]
         public string Gender { get; set; }
     }
 }
